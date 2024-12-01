fd9fcc9e03d7165fbd7321e0c4430cae4c4ba364
AdrianPRG <alopgal962@g.educaand.es>
2024-12-01 20:17:53 +0100

    Correcion de errores

--- operaciones.py
+++ operaciones.py
@@ -1,29 +1,34 @@
 def sumar(num1,num2):
-    if(type(num1)==int and type(num2)==int or type(num1)==float and type(num2==float)):
-        print("La suma es" ,  (num1+num2))
-    else:
-       print("Alguno no es entero")
+    try:
+        print("La suma es " , float(num1) + float(num2))
+    except ValueError:
+        print("Algun elemento no es numerico")
 
 def restar(num1,num2):
-    if(type(num1)==int and type(num2)==int or type(num1)==float and type(num2==float)):
-        print("La resta es" ,  (num1-num2))
-    else:
-       print("Alguno no es entero")
+    try:
+        print("La resta es " , float(num1) - float(num2))
+    except ValueError:
+        print("Algun elemento no es numerico")
 
 def multiplicacion(num1,num2):
-    resultado = 0
-    if(type(num1)==int and type(num2)==int or type(num1)==float and type(num2==float)):
-        for i in range(0,num2):
-            resultado = resultado + num1
-        print("La multiplicacion es ", resultado)
-    else:
-       print("Alguno no es entero")
+    resultado = 0.0
+    try:
+        for i in range(0,int(num2)):
+            resultado = resultado + float(num1)
+        print("La multiplicacion es", resultado)
+    except ValueError:
+        print("Algun elemento no es numerico")
 
 def dividir(num1, num2):
-    cociente = 0
-    for _ in range(int(num1)): 
-        if num1 >= num2:
-            num1 -= num2
-            cociente += 1
-    print("La división es:", cociente)
-
+    try:
+        cociente = 0.0
+        num1 = float(num1)
+        num2 = float(num2)
+        for x in range(int(num1)): 
+            if num1 >= num2:
+                num1 -= num2
+                cociente += 1.0
+        print("La división es:", cociente)
+    except ValueError:
+        print("Algun elemento no es numerico")
+   
